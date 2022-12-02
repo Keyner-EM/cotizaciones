85bcdba9dd7503636841e42083e0d6ee79083855
Keyner-EM <kaestrada67@misena.edu.co>
2022-12-01 19:29:36 -0500

    sistema

--- libreria/urls.py
+++ libreria/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('cotizacion', views.cotizacion, name='cotizaciones'),
     path('cotizaciones', views.cotizaciones, name='cotizaciones'),
     path('formulario', views.formulario, name='formulario'),
-    path('form', views.form, name='form'),
-    path('lavadora lg 19 kg 42 libras color', views.lavadora_lg_19_kg_42_libras_color, name='lavadora')
+    path('lavadora lg 19 kg 42 libras color', views.lavadora_lg_19_kg_42_libras_color, name='lavadora'),
+    path('Lavadora Mabe 16kg 35 libras color gris', views.Lavadora_Mabe_16kg_35_libras_color_gris, name='Lavadora Mabe 16kg 35 libras color gris'),
+    path('Equipo LG', views.Equipo_LG, name='Equipo LG'),
+    path('Estufa 51cm', views.Estufa_51cm, name='Estufa'),
+    path('Estufa mabe 51cm', views.Estufa_mabe_51cm, name='Estufa mabe 51cm'),
+    path('Nevecon Lg 508 lts no frost', views.Nevecon_Lg_508_lts_no_frost, name='Nevecon Lg 508 lts no frost'),
+    path('Nevera Challenger 317 lts no frost', views.Nevera_Challenger_317_lts_no_frost, name='Nevera Challenger 317 lts no frost'),
+    path('Nevera Mabe RMP405FYCU', views.Nevera_Mabe_RMP405FYCU, name='Nevera Mabe RMP405FYCU'),
+    path('Televisor LG 32', views.Televisor_LG_32, name='Televisor LG 32'),
+    path('Televisor LG 50', views.Televisor_LG_50, name='Televisor LG 50'),
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -1,22 +1,116 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 # Create your views here.
 
-def inicio(request):
-    return HttpResponse("<h1>Bienvenido Keyner</h1>")
-def cotizacion(request):
-    return render(request, 'paginas/cotizacion.html')
+
 def cotizaciones(request):
-    return render(request, 'Cotizaciones/index.html')
+    if request.method == 'GET':
+        cotizaciones = models.cotizacines.objects.all()
+        id = request.GET.get("id")
+        if id:
+            cotizaciones.filter(id=id).delete()
+        return render(request, 'Cotizaciones/index.html', {"cotizaciones": cotizaciones})
 
-def formulario(request, *args, **kwargs):
-    print(request, args, kwargs)
-    with open("./loger.txt", "w") as f:
-        f.write("\nrequest:"+str(request)+"\nargs:"+str(args)+"\nkwargs:"+str(kwargs))
-    return render(request, 'paginas/formulario_compra.html', )
-
-def form(request):
-    return render(request, 'cotizaciones/from.html')
-
+def formulario(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/formulario_compra.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+        
 def lavadora_lg_19_kg_42_libras_color(request):
-    return render(request, 'paginas/lavadora lg 19 kg 42 libras color.html')
+    if request.method == 'GET':
+        return render(request, 'paginas/lavadora lg 19 kg 42 libras color.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+    
+def Lavadora_Mabe_16kg_35_libras_color_gris(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Lavadora Mabe 16kg 35 libras color gris.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+    
+def Equipo_LG(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Equipo LG.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+    
+def Estufa_51cm(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Estufa 51cm.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+    
+def Estufa_mabe_51cm(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Estufa mabe 51cm.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+    
+def Estufa_mabe_51cm(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Estufa mabe 51cm.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+
+def Nevecon_Lg_508_lts_no_frost(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Nevecon Lg 508 lts no frost.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+    
+    
+def Nevera_Challenger_317_lts_no_frost(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Nevera Challenger 317 lts no frost.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+    
+
+
+def Nevera_Mabe_RMP405FYCU(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Nevera Mabe RMP405FYCU.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+    
+    
+def Televisor_LG_32(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Televisor LG 32.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+
+
+def Televisor_LG_50(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/Televisor LG 50.html')
+    elif request.method == 'POST':
+        data = {k: v[0] for k, v in dict(request.POST).items()}
+        models.cotizacines(**data).save()
+        return HttpResponse(status = 200)
+    
+    
